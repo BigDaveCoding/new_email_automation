53958b0ef3c309b5953c55196e73e19c0faa112d
BigDaveCoding <bigdavecoding@gmail.com>
2025-01-12 17:27:43 +0000

    get email ids
    created a function that selects the inbox and returns all the email_ids

--- automation_four.py
+++ automation_four.py
@@ -6,6 +6,7 @@ from email.header import decode_header
 import json
 
 
+mail = imaplib.IMAP4_SSL('imap.gmail.com')
 
 def login():
     with open('email_credentials.json', 'r') as file:
@@ -18,10 +19,8 @@ def login():
         exit()
 
     try:
-        mail = imaplib.IMAP4_SSL('imap.gmail.com')
         mail.login(username, password)
         if mail.state == 'AUTH':
-            print(mail.state)
             print('Login Successful')
         else:
             print('Login Failed')
@@ -32,9 +31,17 @@ def login():
 
 
 def get_email_ids():
-    pass
-
-
+    mail.select('inbox')
+    status, messages = mail.search(None, 'ALL')
+    print(status)
+    if status == 'OK':
+        email_ids = messages[0].split()
+        print(len(email_ids))
+        return email_ids
+    else:
+        print('Failed to search emails')
+        return []
+    
 
 login()
-
+email_ids = get_email_ids()
